fdcde74ead1ff646a8398d590d2cd0e57fc662af
sofiscueto <sofiacuetobarros@gmail.com>
2026-07-23 17:58:33 -0300

    Alterações no login

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -2,6 +2,9 @@ import { useState, type FormEvent } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { User, Lock, Loader2, Sun, Moon } from 'lucide-react'
 import { api } from '../api/api'
+import Marca from '../components/marca'
+
+<Marca size={176} />
 
 function Login() {
     const navigate = useNavigate()
